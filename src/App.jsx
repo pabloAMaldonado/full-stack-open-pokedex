634bd29be128113dcc9c5d3b34968530d035fa8b
pabloamaldonado <xpabliito@gmail.com>
2025-02-25 09:48:13 -0300

    2nd added

--- src/App.jsx
+++ src/App.jsx
@@ -18,7 +18,7 @@ const App = () => {
   const match = useMatch('/pokemon/:name')
   const { data: pokemonList, error, isLoading } = useApi('https://pokeapi.co/api/v2/pokemon/?limit=50', mapResults)
 
-  if (isoading) {
+  if (isLoading) {
     return <LoadingSpinner />
   }
   if (error) {
